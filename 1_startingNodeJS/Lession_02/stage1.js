8b4bac0e4f8841455a1fa88f0c22f36198ddee50
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-15 20:39:09 +0000

    Core Modules

---
+++ 1_startingNodeJS/Lession_02/stage1.js
@@ -0,0 +1,17 @@
+// 1.Explain how does the internet work in your own words.
+
+// when we inter to the web browser and try to search something then we inter some URL in 
+// search bat it take that URL and match with their domain name. domain name is just an 
+// IP address it take that response and request to that IP address to give the output in Jason 
+// Format or In HTML its a meta information which is attached to response which describe what 
+// inside in that web page. and we see the web page.
+
+// 2.What are the core modules of node js? Explain the use of each one of them in detail.
+
+// http: Used for creating HTTP servers and making HTTP requests.
+// https: Similar to the http module, but for creating HTTPS servers and making secure HTTPS requests.
+// path: Handles file paths and provides utilities for working with file and directory paths.
+// fs (File System): Provides methods for interacting with the file system, such as reading or writing files.
+// os (Operating System): Provides information about the operating system.
+
+
