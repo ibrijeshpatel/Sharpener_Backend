53f07b3460663cdb0a20f802e19f115c46b0525b
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-17 17:47:08 +0000

    Create first page in clintServer

--- KickstartingNodeJs/question_07.js
+++ KickstartingNodeJs/question_07.js
@@ -8,12 +8,26 @@
 
 // server.listen(3000); // 3000 is port which you have to use
 
+// Get Sequence data 
+// const http = require('http');
+
+// const server1 = http.createServer((req, res) => {
+//     console.log(req.url, req.method, req.headers); // it give sequence value which you have written there in sequence
+//     // process.exit(); // to reset the code
+// });
+// server1.listen(3000); 
 
 const http = require('http');
 
-const server1 = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers); // it give sequence value which you have written there in sequence
+const server2 = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers); 
     // process.exit(); // to reset the code
-});
+    res.setHeader('Content-Type','text/html')  // Response Object
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>My Node JS server</h1></body>');
+    res.write('<html>');
+    res.end();  // after end never write any res.write it give error
+});  // it print My Node JS server in top of the Page
 
-server1.listen(3000); 
+server2.listen(3000); 
