5afff561846b04944ee68e742818ba8af38391fd
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-17 11:16:26 +0000

    Creating Server in Node JS

--- KickstartingNodeJs/question_06_to_08.js
+++ KickstartingNodeJs/question_06_to_08.js
@@ -1,5 +1,6 @@
-// 1.Explain how does the internet work in your own words.
+//Question-06
 
+// 1.Explain how does the internet work in your own words.
 // when we inter to the web browser and try to search something then we inter some URL in 
 // search bat it take that URL and match with their domain name. domain name is just an 
 // IP address it take that response and request to that IP address to give the output in Jason 
@@ -13,5 +14,42 @@
 // path: Handles file paths and provides utilities for working with file and directory paths.
 // fs (File System): Provides methods for interacting with the file system, such as reading or writing files.
 // os (Operating System): Provides information about the operating system.
+
+
+// http Modules // we have to import it
+
+//const http = require("http") // special keyword or special function with node js expose globaly you can run any code default by a node js // through require keyword
+
+//http.createServer();   // access function or method of an object in JS // request listner as an arhument // its an function which excute every incoming request
+
+
+// create a server its take request Listner as an argument and requestListner is a function which excute every incoming request
+
+
+// const http = require('http');
+
+// function requireListner(req, res) {    // it take two argument one is incoming msg and another is response object
+
+// }
+
+// http.createServer(requireListner);
+
+// Another way to create Server // Anonomous Function
+ 
+// const http = require('http');
+
+// http.createServer(function(req, res) {
+
+// });
+
+// Another way to create Server // Arrow Function
+ 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+});
+
+server.listen(3000); // 3000 is port which you have to use
 
 
