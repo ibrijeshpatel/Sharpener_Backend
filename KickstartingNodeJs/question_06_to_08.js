bfd6e8243874f5d6776c20d4eb4875c4e816d2c1
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-17 12:11:02 +0000

    Question Answer

--- KickstartingNodeJs/question_06_to_08.js
+++ KickstartingNodeJs/question_06_to_08.js
@@ -2,7 +2,7 @@
 
 // 1.Explain how does the internet work in your own words.
 // when we inter to the web browser and try to search something then we inter some URL in 
-// search bat it take that URL and match with their domain name. domain name is just an 
+// search but it take that URL and match with their domain name. domain name is just an 
 // IP address it take that response and request to that IP address to give the output in Jason 
 // Format or In HTML its a meta information which is attached to response which describe what 
 // inside in that web page. and we see the web page.
@@ -14,6 +14,26 @@
 // path: Handles file paths and provides utilities for working with file and directory paths.
 // fs (File System): Provides methods for interacting with the file system, such as reading or writing files.
 // os (Operating System): Provides information about the operating system.
+
+// 4. What are relative path and absolute path? How to define them?
+// Absolute path is a path where we can specify all location or path from beginning and provide full path to file or directory.
+// Relative path is a path where we can specify the current path its not start from beginning but it is a current path. 
+
+// 5. What does createServer do?
+// it create our computer into http server. and http server object can listen our computer port and excute function each time when request is made
+
+// 6. What are anonymous functions?
+// A function declair without any function name.
+
+// 7. What do you think server.listens exactly do?Why do we need it?
+// server.listen() is a method used to make a server listen for incoming requests on a 
+// specified network address (host) and port number.
+
+// When you create a server to handle web requests, it needs to actively listen for incoming requests from clients.
+
+// server.listen() takes two main parameters: the port number and an optional host address.
+// The port number is crucial because it helps the server differentiate between different services or applications running on the same machine.
+// The host address specifies the network interface on which the server should listen. 
 
 
 // http Modules // we have to import it
@@ -51,5 +71,8 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000); // 3000 is port which you have to use
+
+
+
 
 
