98d973ec44d0696b765fe3fee2c9d3269d57336c
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-18 14:41:55 +0000

    Creating Links with Links Details

--- KickstartingNodeJs/question_08.js
+++ KickstartingNodeJs/question_08.js
@@ -1,3 +1,5 @@
+/*
+
 const http = require('http');
 
 const server2 = http.createServer((req, res) => {
@@ -30,3 +32,101 @@ const server2 = http.createServer((req, res) => {
 });  
 
 server2.listen(3000); 
+
+*/
+
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+    const getUrl = url.parse(req.url, true);
+
+
+    if (getUrl.pathname == "/") {
+
+        // Home Page
+        res.writeHead(200, {"Content-Type": "text/html; charset=utf-8" });
+        res.write("<html>");
+
+        res.write("<head>");
+        res.write("<title>Home Page</title>");
+
+        res.write("<Style>");
+        res.write("a {margin-right: 10px;}");
+        res.write("</Style>");
+
+        res.write("</head>");
+
+        res.write("<body>");
+        res.write("<h1>Welcome to My Node JS Project</h1>");
+        res.write("<div>");
+        res.write("<a href='/about'>About</a>");
+        res.write("<a href='/node'>Node</a>");
+        res.write("</div>");
+        res.write("<p>हतो वा प्राप्यसि स्वर्गम्, जित्वा वा भोक्ष्यसे महिम्।</p>");
+        res.write("<p>तस्मात् उत्तिष्ठ कौन्तेय युद्धाय कृतनिश्चय:॥</p>");
+        res.write("</body>");
+
+        res.write("</html>");
+        res.end();
+
+    } else if (getUrl.pathname == "/about") {
+
+        // About Page
+        res.writeHead(200, {"Content-Type": "text/html; charset=utf-8" });
+        res.write("<html>");
+
+        res.write("<head>");
+        res.write("<title>About Page</title>");
+
+        res.write("<Style>");
+        res.write("a {margin-right: 10px;}");
+        res.write("</Style>");
+
+        res.write("</head>");
+
+        res.write("<body>");
+        res.write("<h1>Welcome to About Us </h1>");
+        res.write("<div>");
+        res.write("<a href='/'>Home</a>");
+        res.write("<a href='/node'>Node</a>");
+        res.write("</div>");
+        res.write("<p>कर्मण्येवाधिकारस्ते मा फलेषु कदाचन।</p>");
+        res.write("<p>मा कर्मफलहेतुर्भूर्मा ते सङ्गोऽस्त्वकर्मणि॥</p>");
+        res.write("</body>");
+
+        res.write("</html>");
+        res.end();
+
+    } else if (getUrl.pathname == "/node") {
+        
+        // Node Page
+        res.writeHead(200, {"Content-Type": "text/html; charset=utf-8" });
+        res.write("<html>");
+
+        res.write("<head>");
+        res.write("<title>Node Page</title>");
+
+        res.write("<Style>");
+        res.write("a {margin-right: 10px;}");
+        res.write("</Style>");
+
+        res.write("</head>");
+
+        res.write("<body>");
+        res.write("<h1>Welcome to My Node Js projects </h1>");
+        res.write("<div>");
+        res.write("<a href='/'>Home</a>");
+        res.write("<a href='/about'>About</a>");
+        res.write("</div>");
+        res.write("<p>यदा यदा हि धर्मस्य ग्लानिर्भवति भारत:।</p>");
+        res.write("<p>अभ्युत्थानमधर्मस्य तदात्मानं सृजाम्यहम्॥</p>");
+        res.write("</body>");
+
+        res.write("</html>");
+        res.end();
+    }
+
+});
+
+server.listen(3000); 
