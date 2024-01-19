b928dc1417853c6bed43c892d6a7da71c5d97eb2
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-01-19 10:45:19 +0000

    Stream and Buffer

---
+++ KickstartingNodeJs/7_parsingRequest.js
@@ -0,0 +1,15 @@
+// stream 
+
+// streams:- stream is a sequence of data that is move from one point to another.
+// stream of data transfer one file to another within the same computer
+
+// Process- stream of data in chunks(small amount) as they arrive instead of 
+// waiting for the entire data to be available before processing
+
+// Buffers
+
+// Buffers:- waiting for data to excute the process or extra data which is 
+// watting for excuite post data and their turn on so the data is in que that data is called buffer.
+
+// ex:- 100 people come to join the roler coster but roler coster has 30 seat so 30 people sit on roler coster and remaining 70 is in que that is buffer 
+// similary only 10 people come but coster has 30 seat so that 10 people waiting for 20 people to fill the seat than coster start. that 10 people is waiting so that people is also buffer.
