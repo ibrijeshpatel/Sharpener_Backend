a46b86aa4bb6fa510a8fecb9a3df3be614efb02e
Brijesh Patel <136248611+Brjs-Ptl@users.noreply.github.com>
2024-02-01 17:51:32 +0530

    Q 1

---
+++ StartingExpressFramwork/1startedwitExpress.js
@@ -0,0 +1,3 @@
+// Express Js is a fast, unopinionated, minimalist web framework for Node. js that provides a 
+// robust set of features for web and mobile applications. It has many advantages, 
+// such as simplicity, flexibility, community support, scalability, and cross-platform functionality.
