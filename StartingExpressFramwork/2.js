d945b7fbe0bc6d1efa481a5bc8f98f45e785c8fb
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-02-01 20:23:26 +0530

    Single Input as per Youtube

--- StartingExpressFramwork/2.js
+++ StartingExpressFramwork/2.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 // here we write code down to up side because in app.use method next is not working if we try to reflect both places page
+app.use("/add_product",(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add Products </button></input></form>')    
+    });
+
 app.use("/product",(req, res, next) => {
-    console.log("Product Page");
-    res.send("<h1>Welcome to Product Page</h1>")
+    console.log(req.body);
+    res.redirect("/");
 });
 
 app.use("/",(req, res, next) => {
-    console.log("This is Main Page");
     res.send("<h1>Welcome to main page of this Web</h1>")
     next();
 });
