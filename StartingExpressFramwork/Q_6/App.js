79ef1d2dda4eefadd9fd3d4402a4b3f36dfc8e7f
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-02-21 20:09:59 +0530

    Updated CSS

--- StartingExpressFramwork/Q_6/App.js
+++ StartingExpressFramwork/Q_6/App.js
@@ -5,16 +5,17 @@ const path = require("path")
 
 const errorControler = require("../Q_6/controler/controlError");
 
-const adminRoutes = require('./Routes/admin');
+const productsRoutes = require('./Routes/products');
 const shopRoutes = require("./Routes/shop");
 const contactRoutes = require("./Routes/contact");
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));  // not require
 
-app.use('/admin', adminRoutes);
+app.use('/products', productsRoutes);
 app.use(shopRoutes);
-app.use('/admin', contactRoutes);
-app.use('/admin', contactRoutes);
+app.use('/products', contactRoutes);
+app.use('/products', contactRoutes);
 
 // app.use(errorControler.get404);
 app.use(errorControler.get404);
--- StartingExpressFramwork/Q_6/controler/controldata.js
+++ StartingExpressFramwork/Q_6/controler/controldata.js
@@ -3,7 +3,7 @@ const path = require("path")      // path require for finding the path
 const rootDir = require("../path_utility/path") // access the path where rootDir is available
  
 exports.getAddProducts = (req, res) => {                   // exporting data and then sending with their name to access this data in their respective file
-    res.sendFile(path.join(rootDir, "html", "admin.html")); 
+    res.sendFile(path.join(rootDir, "html", "products.html")); 
 };
 
 exports.postAddProducts = (req, res) => {
